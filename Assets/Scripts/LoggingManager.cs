4d85f38acda9f33813ad0c6004fab771517fe5ab
Seungchan Jeong cliwo@naver.com <cliwo@naver.com>
2018-07-03 15:38:24 +0900

    In progress 'Polishing Data File Process'

---
+++ Assets/Scripts/LoggingManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoggingManager : MonoBehaviour {
+
+	public string patientID;
+
+	void Awake()
+	{
+		DontDestroyOnLoad(gameObject);
+	}
+	public void OnFlush()
+	{
+		if(IsValid(patientID))
+		{
+
+		}
+		else
+		{
+			Debug.LogWarning("Patient ID가 정상적이지 않습니다. 확인해주세요");
+		}
+	}
+
+	bool IsValid(string id)
+	{
+		return true;
+	}
+}
---
+++ Assets/Scripts/LoggingManagerEditor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(LoggingManager))] //? 이거 왜이럼
+[CanEditMultipleObjects]
+public class LoggingManagerEditor : Editor {
+
+	SerializedProperty patientID;
+	void OnEnable()
+    {
+        patientID = serializedObject.FindProperty("patientID");
+    }
+
+    public override void OnInspectorGUI()
+    {
+		serializedObject.Update();
+		EditorGUILayout.PropertyField(patientID);
+        serializedObject.ApplyModifiedProperties();
+		if(GUILayout.Button("Make Data File"))
+		{
+			Debug.Log("Make Data File");
+		}
+    }
+
+    public void OnSceneGUI()
+    {
+
+    }
+}
