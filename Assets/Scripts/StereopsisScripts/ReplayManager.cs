084c96c1bac30805ba377c13435a307b6318d2a0
Seungchan Jeong cliwo@naver.com <cliwo@naver.com>
2018-07-12 22:31:42 +0900

    Edit ReplayManager

--- Assets/Scripts/StereopsisScripts/ReplayManager.cs
+++ Assets/Scripts/StereopsisScripts/ReplayManager.cs
@@ -7,22 +7,56 @@ using UnityEngine;
 
 public class ReplayManager : MonoBehaviour {
     //The order of Objects should be same on Replay Scene and Stereopsis Scene.
+    
+    public static ReplayManager instance;
 	public List<GameObject> serializedObjects;
+    public Mesh _sphereMesh;
+    public Material _material;
     List<CustomTransform> frameData;
+
+    [HideInInspector]
+    public HandSerializer left_hand;
+    [HideInInspector]
+    public HandSerializer right_hand;
 
     bool enableReplay = false;
     int cursor = 0;
 
+    void Awake()
+    {
+        if(instance == null) {
+            instance = this;
+        }
+        else {
+            Destroy(this);
+        }
+
+        if(!enableReplay)
+        {
+
+        }
+    }
 	void Update()
 	{
         if(enableReplay && frameData != null)
         {
-            for (int i = 0 ; i < serializedObjects.Count; i ++)
+            while(true)
             {
+                int currentFrame = frameData[cursor].frame;
                 CustomTransform currentFrameData = frameData[cursor++]; 
-                serializedObjects[currentFrameData.index].transform.position = currentFrameData.position;
-                serializedObjects[currentFrameData.index].transform.rotation = Quaternion.Euler(currentFrameData.rotation);
-                serializedObjects[currentFrameData.index].transform.localScale = currentFrameData.scale;
+                if(currentFrameData.index < serializedObjects.Count)
+                {
+                    serializedObjects[currentFrameData.index].transform.position = currentFrameData.position;
+                    serializedObjects[currentFrameData.index].transform.rotation = Quaternion.Euler(currentFrameData.rotation);
+                }
+                else
+                {
+                    Graphics.DrawMesh(_sphereMesh, Matrix4x4.TRS(currentFrameData.position,
+                    Quaternion.identity, Vector3.one/100), _material, 0);
+                }
+
+                if(currentFrame != frameData[cursor].frame)
+                    return;
             }
         }
         else
@@ -38,9 +72,22 @@ public class ReplayManager : MonoBehaviour {
         file.Directory.Create();
         using (var writer = new StreamWriter(filePath, append: true))
         {
-            for(int i = 0; i < serializedObjects.Count; i++)
+            int i;
+            for(i = 0; i < serializedObjects.Count; i++)
             {
                 writer.WriteLine(SerializeTransfrom(i, serializedObjects[i].transform));
+            }
+            if(left_hand != null)  {
+                List<Vector3> positions = left_hand.GetHandPositions();
+                foreach(Vector3 pos in positions) { 
+                    writer.WriteLine(SerializeTransfrom(i++, pos));
+                }
+            }
+            if(right_hand != null)  {
+                List<Vector3> positions = right_hand.GetHandPositions();
+                foreach(Vector3 pos in positions) { 
+                    writer.WriteLine(SerializeTransfrom(i++, pos));
+                }
             }
             writer.Flush();
         }
@@ -54,6 +101,14 @@ public class ReplayManager : MonoBehaviour {
         ,tr.rotation.x, tr.rotation.y, tr.rotation.z
         ,tr.localScale.x , tr.localScale.y, tr.localScale.z);
     }
+    string SerializeTransfrom(int index, Vector3 pos)
+    {
+        return String.Format("{0},{1},{2},{3},{4},{5},{6},{7},{8},{9}",
+        index
+        ,pos.x, pos.y, pos.z
+        ,0 , 0 , 0
+        ,1, 1, 1);
+    }
 
     public void LoadReplayData()
     {
@@ -64,6 +119,7 @@ public class ReplayManager : MonoBehaviour {
         {
             frameData = new List<CustomTransform>();
             string line = reader.ReadLine();
+            int frameCount = 0;
             while(line != null && line.Length != 0)
             {
                 string[] splitArray = line.Split(',');
@@ -76,6 +132,10 @@ public class ReplayManager : MonoBehaviour {
                 tr.position = position;
                 tr.rotation = rotation;
                 tr.scale = scale;
+
+                if(tr.index == 0)
+                    frameCount ++;
+                tr.frame = frameCount;
 
                 frameData.Add(tr);
 
@@ -92,6 +152,7 @@ public class ReplayManager : MonoBehaviour {
 
     class CustomTransform
     {
+        public int frame;
         public int index;
         public Vector3 position;
         public Vector3 rotation;
