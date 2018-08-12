785d256d1d7c298e5c44c5bae55ac71144c1a597
DESKTOP-N3UA55V\Chan <cliwo@naver.com>
2018-08-13 01:54:58 +0900

    Stereopsis with Environment

--- Assets/Scripts/StereopsisScripts/PositioningObjects.cs
+++ Assets/Scripts/StereopsisScripts/PositioningObjects.cs
@@ -4,25 +4,65 @@ using UnityEngine;
 using Fove.Managed;
 
 public class PositioningObjects : MonoBehaviour {
-  public GameObject globalDirectionalLight;
+    public GameObject globalDirectionalLight;
+    public GameObject positionSocket;
+    public GameObject environment;
 
-  GameObject foveInterface;
+    GameObject foveInterface;
 
 	void Awake()
 	{
-		foveInterface = SceneLoader.instance.foveInterface.GetComponentInChildren<FoveInterface>().gameObject;
-
-    float forwardAmount = 0.55f;
-
-    transform.position = FoveInterface.GetHMDRotation() * Vector3.forward * forwardAmount;
-    transform.position = new Vector3(transform.position.x + foveInterface.transform.position.x,
-                                    foveInterface.transform.position.y,
-                                    transform.position.z + foveInterface.transform.position.z);
-    transform.LookAt(foveInterface.transform.position);
-
-    // Light configuration
-    globalDirectionalLight.transform.position = transform.position;
-    globalDirectionalLight.transform.rotation = transform.rotation;
-    globalDirectionalLight.transform.Rotate(new Vector3(30, 30, 0));
+        foveInterface = SceneLoader.instance.foveInterface.GetComponentInChildren<FoveInterface>().gameObject;
+        AllignWithEnvironment();	
 	}
+
+    void AllignWithNoEnvironment()
+    {
+        float forwardAmount = 0.55f;
+
+        transform.position = FoveInterface.GetHMDRotation() * Vector3.forward * forwardAmount;
+        transform.position = new Vector3(transform.position.x + foveInterface.transform.position.x,
+                                        foveInterface.transform.position.y,
+                                        transform.position.z + foveInterface.transform.position.z);
+        transform.LookAt(foveInterface.transform.position);
+
+        // Light configuration
+        globalDirectionalLight.transform.position = transform.position;
+        globalDirectionalLight.transform.rotation = transform.rotation;
+        globalDirectionalLight.transform.Rotate(new Vector3(30, 30, 0));
+    }
+
+    void AllignWithEnvironment()
+    {
+        AllignWithNoEnvironment();
+
+        Quaternion gazeDir = FoveInterface.GetHMDRotation();
+        Matrix4x4 rotTranspose = new Matrix4x4(new Vector4(1, 0, 0, 0),
+            new Vector4(0, 1, 0, 0),
+            new Vector4(0, 0, 1, 0),
+            new Vector4(0,
+            -positionSocket.transform.rotation.eulerAngles.y + gazeDir.eulerAngles.y,
+            0, 1));
+
+        Vector3 newRot = rotTranspose * new Vector4(environment.transform.rotation.eulerAngles.x,
+            environment.transform.rotation.eulerAngles.y,
+            environment.transform.rotation.eulerAngles.z, 1);
+        environment.transform.rotation = Quaternion.Euler(newRot.x, newRot.y, newRot.z);
+
+
+        Matrix4x4 transpose = new Matrix4x4(new Vector4(1, 0, 0, 0),
+            new Vector4(0, 1, 0, 0),
+            new Vector4(0, 0, 1, 0),
+            new Vector4(- positionSocket.transform.position.x + foveInterface.transform.position.x,
+            - positionSocket.transform.position.y + foveInterface.transform.position.y,
+            - positionSocket.transform.position.z + foveInterface.transform.position.z, 1));
+        Debug.Log(transpose);
+
+        
+        Vector3 newPos = transpose * new Vector4(environment.transform.position.x, environment.transform.position.y, environment.transform.position.z , 1);
+        Debug.Log(newPos);
+        environment.transform.position = newPos;
+
+    }
+
 }
